98f4dc5b1dcf9d7d89564f7b1cd4214cf6a74765
sigmavcs <jchamath+sigma@gmail.com>
2019-08-20 17:51:55 +0530

    1-05

--- sigma-698/lambda.js
+++ sigma-698/lambda.js
@@ -1,4 +1,20 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = function (event, context, callback) {
+    sns.publish({
+        Message: 'Hi, how are you?',
+        Subject: 'Hello',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
     // user 2
     callback(null, { "message": "Successfully executed" });
 }
